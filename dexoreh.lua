283000739ce5c4adbd6ca7df668d4bc0599cf100
MrCatMemes <l.berndt2020@gmail.com>
2025-10-26 21:24:29 +0100

    Update dexoreh.lua

--- dexoreh.lua
+++ dexoreh.lua
@@ -1,2 +1,1 @@
-
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/b691305ab78e76e4160835e40b5d2b710f724c6ca98602e16f55b002e8977f06/download"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/fd4f96495c315879"))()
