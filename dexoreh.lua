e9f08527f007e4cd18779160226fe5789bb3c12f
MrCatMemes <l.berndt2020@gmail.com>
2025-11-16 13:38:51 +0100

    Update dexoreh.lua

--- dexoreh.lua
+++ dexoreh.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5ba740dd04af1ce564943260dee76ff6.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c9ad5576cae43cd9c84ddf71775db412.lua"))()
