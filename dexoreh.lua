6c38f75babfb77fd87588a09e9811af4c6ea423d
MrCatMemes <l.berndt2020@gmail.com>
2026-03-21 21:56:35 +0100

    Update Lua script loader URL

--- dexoreh.lua
+++ dexoreh.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/212410690c813b5d0c7224b0973aa648.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d9dcdda822bbd5328210443d34485c09.lua"))()
